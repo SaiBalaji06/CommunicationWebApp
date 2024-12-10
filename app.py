eec6d9fc586d51abe130bb8b9f8dc4e9cc9fa3aa
SaiBalaji06 <saibalajiuddagiri2004@gmail.com>
2024-12-11 00:08:25 +0530

    first-commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '83fbefaebddbfa5c6230efaaf3762e27f3f4c02751bbc2c0c9ce3aa1011695cd'
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('message')
+def handle_message(msg):
+    print(f"Message received: {msg}")
+    send(f"Server: {msg}", broadcast=True)  # Echo back the message to all clients
+
+if __name__ == '__main__':
+    socketio.run(app)
